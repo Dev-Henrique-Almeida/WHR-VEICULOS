13983c0aca0ace1028b119a381abc18110d56e9c
Henrique <henrique.almeida.dev@gmail.com>
2022-09-01 14:39:22 -0300

    Corrigindo o SpringBoot

---
+++ src/main/java/com/example/WHRVEICULOS/WhrVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.example.WHRVEICULOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhrVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhrVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WHRVEICULOS/WhrVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WHRVEICULOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhrVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
