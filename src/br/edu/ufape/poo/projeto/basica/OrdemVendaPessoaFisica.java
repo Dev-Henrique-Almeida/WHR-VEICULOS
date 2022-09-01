3547090befe3d323dd38b18f145f78e8eee01c48
Henrique <henrique.almeida.dev@gmail.com>
2022-08-31 22:56:45 -0300

    Atualizacao dos repositorios - Feito pelos 3 membros

--- src/br/edu/ufape/poo/projeto/basica/OrdemVendaPessoaFisica.java
+++ src/br/edu/ufape/poo/projeto/basica/OrdemVendaPessoaFisica.java
@@ -3,7 +3,7 @@ package br.edu.ufape.poo.projeto.basica;
 import java.util.Date;
 
 public class OrdemVendaPessoaFisica extends OrdemVenda {
-	
+
 	private ClienteFisico cliente;
 	public OrdemVendaPessoaFisica(float valor, Veiculo veiculo, boolean novo, Date dataOperacao, String formaPagamento,
 			boolean pago, boolean vendaConcluida, String codVenda, Funcionario vendedor, long id, ClienteFisico cliente) {
---
+++ src/br/edu/ufape/poo/projeto/repositorio/RepositorioVeiculoArrayList.java
@@ -0,0 +1,52 @@
+package br.edu.ufape.poo.projeto.repositorio;
+
+import java.util.List;
+
+import br.edu.ufape.poo.projeto.basica.Modelo;
+import br.edu.ufape.poo.projeto.basica.Veiculo;
+
+public class RepositorioVeiculoArrayList implements RepositorioVeiculo {
+
+	@Override
+	public void adicionarVeiculo(Veiculo v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Veiculo procurarModeloVeiculo(String modelo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float procurarVeiculoEntre(float a, float b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Veiculo> listarVeiculosNovos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Veiculo> listarVeiculosUsados() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Modelo> listarModelos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Veiculo> findByModelo(Modelo modelo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
