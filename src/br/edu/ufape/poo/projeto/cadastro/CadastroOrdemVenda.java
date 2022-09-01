43166a146111fb47c9fdac7a607507a2fbe543c3
Henrique <henrique.almeida.dev@gmail.com>
2022-09-01 01:04:36 -0300

    Criado o cadastro da OrdemVenda

---
+++ src/br/edu/ufape/poo/projeto/cadastro/CadastroOrdemVenda.java
@@ -0,0 +1,42 @@
+package br.edu.ufape.poo.projeto.cadastro;
+import br.edu.ufape.poo.projeto.repositorio.RepositorioOrdemVenda;
+import br.edu.ufape.poo.projeto.basica.OrdemVendaPessoaJuridica;
+import br.edu.ufape.poo.projeto.basica.Funcionario;
+import br.edu.ufape.poo.projeto.basica.OrdemVenda;
+import br.edu.ufape.poo.projeto.basica.OrdemVendaPessoaFisica;
+import java.util.List;
+
+
+public class CadastroOrdemVenda {
+	
+private RepositorioOrdemVenda repositorioOrdemVenda;
+	
+	public CadastroOrdemVenda(RepositorioOrdemVenda repositorioOrdemVenda) {
+		this.repositorioOrdemVenda = repositorioOrdemVenda;
+	}
+	
+	public RepositorioOrdemVenda getRepositorioOrdemVenda() {
+		return repositorioOrdemVenda;
+	}
+	
+	public void setRepositorioOrdemVenda(RepositorioOrdemVenda repositorioOrdemVenda) {
+		this.repositorioOrdemVenda = repositorioOrdemVenda;
+	}
+	
+	public OrdemVenda procurarOrdemVenda(String codigoVenda) {
+		return repositorioOrdemVenda.procurarOrdemVenda(codigoVenda);
+	}
+
+	public OrdemVenda procurarProcurarVendedor(Funcionario f) {
+		return repositorioOrdemVenda.procurarVendedor(f);
+	}
+
+	public List<OrdemVenda> listarOrdemVendaPessoaJuridica() {
+		return repositorioOrdemVenda.listarOrdemVendaPessoaJuridica();
+	}
+	
+	public List<OrdemVenda> listarOrdemVendaPessoaFisica() {
+		return repositorioOrdemVenda.listarOrdemVendaPessoaFisica();
+	}
+
+}
