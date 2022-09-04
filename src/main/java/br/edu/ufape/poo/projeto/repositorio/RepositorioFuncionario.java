fa5f7305eb2e56f3e3eebf34de81fbafcee71a42
Dev-Henrique-Almeida <henrique.almeida.dev@gmail.com>
2022-09-04 16:49:40 -0300

    Repositorios ClienteJuridico e Funcionario - Atualizados

--- src/main/java/br/edu/ufape/poo/projeto/repositorio/RepositorioFuncionario.java
+++ src/main/java/br/edu/ufape/poo/projeto/repositorio/RepositorioFuncionario.java
@@ -1,11 +1,26 @@
 package br.edu.ufape.poo.projeto.repositorio;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.edu.ufape.poo.projeto.basica.ClienteFisico;
+import br.edu.ufape.poo.projeto.basica.ClienteJuridico;
 import br.edu.ufape.poo.projeto.basica.Funcionario;
+import br.edu.ufape.poo.projeto.basica.Veiculo;
+
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 @Repository
 public interface RepositorioFuncionario extends JpaRepository<Funcionario, Long> {
+
+	Funcionario findbyNome(String nome);
+
+	Funcionario findByCargo(String cargo);
+
+	void deleteByCpf(String cpf);
+
+	Veiculo findbyVeiculo(Veiculo v);
+
+	ClienteFisico save(ClienteFisico entity);
+
+	ClienteJuridico save(ClienteJuridico entity);
 	
 }
