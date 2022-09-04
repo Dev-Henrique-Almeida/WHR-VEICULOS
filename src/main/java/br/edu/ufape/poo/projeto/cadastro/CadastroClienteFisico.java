994fee2cee8ee858728a0077d2899d8853c5630b
WilliamSantos-Dev <williambatista958@gmail.com>
2022-09-04 16:44:41 -0300

    Atualização Clite PF

--- src/main/java/br/edu/ufape/poo/projeto/cadastro/CadastroClienteFisico.java
+++ src/main/java/br/edu/ufape/poo/projeto/cadastro/CadastroClienteFisico.java
@@ -1,16 +1,44 @@
 package br.edu.ufape.poo.projeto.cadastro;
+import br.edu.ufape.poo.projeto.basica.ClienteFisico;
 import br.edu.ufape.poo.projeto.repositorio.RepositorioClienteFisico;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
-
 public class CadastroClienteFisico {
 	
 	@Autowired
 	private RepositorioClienteFisico repositorioClienteFisico;
+
+	public ClienteFisico save(ClienteFisico entity) {
+		return repositorioClienteFisico.save(entity);
+	}
+
+	public List<ClienteFisico> findAll() {
+		return repositorioClienteFisico.findAll();
+	}
+
+	public void delete(ClienteFisico entity) {
+		repositorioClienteFisico.delete(entity);
+	}
+
+	public void deleteByCpf(String cpf) {
+		repositorioClienteFisico.deleteByCpf(cpf);
+	}
+
+	public ClienteFisico findByCpf(String cpf) {
+		return repositorioClienteFisico.findByCpf(cpf);
+	}
 	
+	public ClienteFisico findByNomePF(String nome) {
+		return repositorioClienteFisico.findByNomePF(nome);
+	}
+	
+
+		
 
 }
