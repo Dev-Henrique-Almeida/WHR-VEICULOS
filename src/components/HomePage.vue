f3a069fd6a8b9da235e2ade08e6ae49349c83c76
William <williambatista958@gmail.com>
2022-09-11 19:08:06 -0300

    Inicio Front VueJs

---
+++ src/components/HomePage.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col dark>
+        <v-img
+          :src="require('../assets/logo.png')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+    
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HomePage',
+  }
+</script>
