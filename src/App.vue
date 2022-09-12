3103145834f9f991c5bac1055e356a7bfe814a44
William <williambatista958@gmail.com>
2022-09-11 23:58:14 -0300

    Primeira tela, Lista de clientes, logo e carrosel de imgs

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,17 @@
   <v-app>
     <v-app-bar
       app
-      color="primary"
+      color="black"
       dark
     >
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
+            <v-img
           alt="Vuetify Name"
           class="shrink mt-1 hidden-sm-and-down"
           contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
+          min-width="175"
+          src="./assets/logoWHR.png"
+          width="175"
         />
       </div>
 
@@ -45,16 +36,11 @@
 
 <script>
 import HomePage from './components/HomePage';
-
 export default {
   name: 'App',
 
   components: {
     HomePage,
   },
-
-  data: () => ({
-    //
-  }),
 };
 </script>
